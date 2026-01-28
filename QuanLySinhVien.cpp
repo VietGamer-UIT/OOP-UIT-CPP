481882a2bf8256c237112383f16dac88a5f4f230
Đoàn Hoàng Việt <skyvdygaming@gmail.com>
2026-01-28 12:33:16 +0700

    Thêm dấu tiếng Việt

--- QuanLySinhVien.cpp
+++ QuanLySinhVien.cpp
@@ -1,35 +1,40 @@
 /*
- *Doan Hoang Viet (Viet Gamer)
+ *Đoàn Hoàng Việt (Việt Gamer)
  *Date: 27/01/2026
- *Description: Chuong trinh quan ly sinh vien co ban (OOP C++)
+ *Description: Chương trình quản lý sinh viên cơ bản (OOP C++)
  *Features:
- * 	-Tao ID tu dong (SV001, SV002,...)
- *	-Chuan hoa ho ten
- *	-Sap xep theo GPA giam dan
- *	-Nap chong toan tu Input / Output
+ * 	-Tạo ID tự động (SV001, SV002,...)
+ *	-Chuẩn hóa họ tên
+ *	-Sắp xếp theo GPA giảm dần
+ *	-Nạp chồng toán tử Input / Output
  */
- 
-#include <bits/stdc++.h>
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <iomanip>
+#include <algorithm>
 
 using namespace std;
 
 class SinhVien
 {
-	private:
-		string id, ten, ns;
-		double gpa;
-		static int dem;
-	public:
-		SinhVien ();
-		friend istream& operator >> (istream &in, SinhVien &a);
-		friend ostream& operator << (ostream &out, SinhVien b);
-		friend bool operator < (SinhVien a, SinhVien b);
-		friend void chuanHoaTen(SinhVien&);
-		friend void chuanHoaNS(string&);
+private:
+	string id, ten, ns;
+	double gpa;
+	static int dem;
+public:
+	SinhVien();
+	friend istream& operator >> (istream& in, SinhVien& a);
+	friend ostream& operator << (ostream& out, SinhVien b);
+	friend bool operator < (SinhVien a, SinhVien b);
+	friend void chuanHoaTen(SinhVien&);
+	friend void chuanHoaNS(string&);
 };
 
-//Ham chuan hoa ten
-void chuanHoaTen(SinhVien &a)
+//Hàm chuẩn hóa tên
+void chuanHoaTen(SinhVien& a)
 {
 	string res = "";
 	stringstream ss(a.ten);
@@ -45,18 +50,18 @@ void chuanHoaTen(SinhVien &a)
 	a.ten = res;
 }
 
-//Ham chuan hoa ngay sinh
-void chuanHoaNS(string &ns)
+//Hàm chuẩn hóa ngày sinh
+void chuanHoaNS(string& ns)
 {
 	if (ns[1] == '/') ns = "0" + ns;
 	if (ns[4] == '/') ns.insert(3, "0");
 }
 
-//Khoi tao bien tinh
+//Khởi tạo biến tĩnh
 int SinhVien::dem = 0;
 
-//Nap chong nhap
-istream& operator >> (istream &in, SinhVien &a)
+//Nạp chồng nhập
+istream& operator >> (istream& in, SinhVien& a)
 {
 	SinhVien::dem++;
 	string s = to_string(SinhVien::dem);
@@ -71,27 +76,27 @@ istream& operator >> (istream &in, SinhVien &a)
 	return in;
 }
 
-//Nap chong xuat
-ostream& operator << (ostream &out, SinhVien a)
+//Nạp chồng xuất
+ostream& operator << (ostream& out, SinhVien a)
 {
 	out << a.id << " " << a.ten << " " << a.ns << " " << fixed << setprecision(2) << a.gpa << endl;
 	return out;
 }
 
-//Constructor mac dinh
+//Constructor mặc định
 SinhVien::SinhVien()
 {
 	id = ten = ns = "";
 	gpa = 0;
 }
 
-//GPA giam dan
+//GPA giảm dần
 bool operator < (SinhVien a, SinhVien b)
 {
 	return a.gpa > b.gpa;
 }
 
-int main ()
+int main()
 {
 	int n; cin >> n;
 	vector<SinhVien> a(n);
