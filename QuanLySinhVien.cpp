34f7b3d439b424c11cc3528faeeafe78e61060b7
Đoàn Hoàng Việt <skyvdygaming@gmail.com>
2026-01-31 13:28:24 +0700

    Add optimized SinhVien management program

--- QuanLySinhVien.cpp
+++ QuanLySinhVien.cpp
@@ -21,87 +21,80 @@ using namespace std;
 class SinhVien
 {
 private:
-	string id, ten, ns;
-	double gpa;
-	static int dem;
+    string id, ten, ns;
+    double gpa;
+    static int dem;
+    //Hàm chuẩn hóa tên
+    void chuanHoaTen() {
+        string res = "";
+        stringstream ss(ten);
+        string temp;
+        while (ss >> temp) {
+            res += toupper(temp[0]);
+            for (int i = 1; i < temp.length(); i++)
+                res += tolower(temp[i]);
+            res += " ";
+        }
+        if (!res.empty()) res.erase(res.length() - 1);
+        ten = res;
+    }
+    //Hàm chuẩn hóa ngày sinh
+    void chuanHoaNS() {
+        if (ns[1] == '/') ns = "0" + ns;
+        if (ns[4] == '/') ns.insert(3, "0");
+    }
 public:
-	SinhVien();
-	friend istream& operator >> (istream& in, SinhVien& a);
-	friend ostream& operator << (ostream& out, SinhVien b);
-	friend bool operator < (SinhVien a, SinhVien b);
-	friend void chuanHoaTen(SinhVien&);
-	friend void chuanHoaNS(string&);
+    //Constructor mặc định
+    SinhVien(string id = "", string ten = "", string ns = "", double gpa = 0)
+    {
+        this->id = id;
+		this->ten = ten;
+		this->ns = ns;
+		this->gpa = gpa;
+    }
+    friend istream& operator >> (istream& in, SinhVien& a);
+    friend ostream& operator << (ostream& out, const SinhVien& a);
+    friend bool operator < (const SinhVien& a, const SinhVien& b);
 };
-
-//Hàm chuẩn hóa tên
-void chuanHoaTen(SinhVien& a)
-{
-	string res = "";
-	stringstream ss(a.ten);
-	string temp;
-	while (ss >> temp)
-	{
-		res += toupper(temp[0]);
-		for (int i = 1; i < temp.length(); i++)
-			res += tolower(temp[i]);
-		res += " ";
-	}
-	if (!res.empty()) res.erase(res.length() - 1);
-	a.ten = res;
-}
-
-//Hàm chuẩn hóa ngày sinh
-void chuanHoaNS(string& ns)
-{
-	if (ns[1] == '/') ns = "0" + ns;
-	if (ns[4] == '/') ns.insert(3, "0");
-}
 
 //Khởi tạo biến tĩnh
 int SinhVien::dem = 0;
 
 //Nạp chồng nhập
-istream& operator >> (istream& in, SinhVien& a)
-{
-	SinhVien::dem++;
-	string s = to_string(SinhVien::dem);
-	while (s.length() < 3)
-		s = "0" + s;
-	a.id = "SV" + s;
-	cout << "Nhap ten: "; in.ignore();
-	getline(in, a.ten); chuanHoaTen(a);
-	cout << "Nhap ngay sinh: "; in >> a.ns;
-	chuanHoaNS(a.ns);
-	cout << "Nhap GPA: "; in >> a.gpa;
-	return in;
+istream& operator >> (istream& in, SinhVien& a) {
+    SinhVien::dem++;
+    string s = to_string(SinhVien::dem);
+    while (s.length() < 3) s = "0" + s;
+    a.id = "SV" + s;
+    cout << "Nhap ten: ";
+    getline(in, a.ten);
+    a.chuanHoaTen();
+    cout << "Nhap ngay sinh: ";
+    in >> a.ns;
+    a.chuanHoaNS();
+    cout << "Nhap GPA: ";
+    in >> a.gpa;
+    in.ignore();
+    return in;
 }
 
 //Nạp chồng xuất
-ostream& operator << (ostream& out, SinhVien a)
-{
+ostream& operator << (ostream& out, const SinhVien& a) {
 	out << a.id << " " << a.ten << " " << a.ns << " " << fixed << setprecision(2) << a.gpa << endl;
-	return out;
-}
-
-//Constructor mặc định
-SinhVien::SinhVien()
-{
-	id = ten = ns = "";
-	gpa = 0;
+    return out;
 }
 
 //GPA giảm dần
-bool operator < (SinhVien a, SinhVien b)
-{
-	return a.gpa > b.gpa;
+bool operator < (const SinhVien& a, const SinhVien& b) {
+    return a.gpa > b.gpa;
 }
 
-int main()
-{
-	int n; cin >> n;
-	vector<SinhVien> a(n);
-	for (int i = 0; i < n; i++) cin >> a[i];
-	sort(a.begin(), a.end());
-	for (SinhVien x : a) cout << x;
-	return 0;
+int main() {
+    int n; cin >> n;
+    cin.ignore();
+    vector<SinhVien> a(n);
+    for (int i = 0; i < n; i++) cin >> a[i];
+    sort(a.begin(), a.end());
+    for (const SinhVien& x : a) cout << x;
+    return 0;
 }
